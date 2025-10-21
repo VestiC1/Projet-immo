26b62ac242d5207803b7eb70bc44487c80cacc2f
Jonathan Caillaux <jonathan.caillaux@protonmail.com>
2025-10-21 10:19:49 +0200

    ADD config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+# Projecty root directory
+ROOT = Path(__file__).parent
+
+# Data directory
+DATA_DIR = ROOT / "data"
+
+# Token database
+TOKENDB = DATA_DIR / "tokendb.json"
